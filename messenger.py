635b433228dd09c6e38adacc2f692a1be48bfe1c
AustinHCopley <acopley@mines.edu>
2023-02-23 09:55:33 -0700

    class

--- messenger.py
+++ messenger.py
@@ -3,6 +3,22 @@ import cv2
 import numpy as np
 from sys import getsizeof
 
+class Fragment():
+
+    def __init__(self, ip="127.0.0.1", port=5005, packsize=6000):
+        self.ip = ip
+        self.port = port
+        self.packsize=6000
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def fragment(self, data):
+        framesize = getsizeof(data)
+        # split the frame into 6KB datagrams
+        for x in range( framesize // self.packsize + 1 ):
+            self.sock.sendto(data[x*self.packsize : x*self.packsize+self.packsize], (self.ip, self.port))
+        self.sock.sendto("xxx".encode(), (self.ip, self.port))
+
+
 IP = "127.0.0.1"
 PORT = 5005
 PACKSIZE = 6000
