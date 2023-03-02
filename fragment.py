d4a0edeec39ce4357df533c87df08836146674b4
AustinHCopley <acopley@mines.edu>
2023-03-01 23:33:15 -0700

    continous frames

--- fragment.py
+++ fragment.py
@@ -1,22 +1,23 @@
 import socket
 import cv2
-import numpy as np
+import pickle
 from sys import getsizeof
 
 class Fragment():
     """fragment an opencv image into smaller datagrams, and send them via udp port"""
-    def __init__(self, ip="127.0.0.1", port=5005, packsize=6000):
+    def __init__(self, ip="127.0.0.1", port=5005, packsize=100000):
         self.ip = ip
         self.port = port
         self.packsize = packsize
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # set buffer size
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, self.packsize)
 
     def fragment(self, data):
         framesize = getsizeof(data)
-        # split the frame into 6KB datagrams
+        # split the frame into datagrams
         for x in range( framesize // self.packsize + 1 ):
             self.sock.sendto(data[x*self.packsize : x*self.packsize+self.packsize], (self.ip, self.port))
-        self.sock.sendto("xxx".encode(), (self.ip, self.port))
 
 
 def main():
@@ -24,11 +25,12 @@ def main():
 
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
+    while ret:
+        ret, frame = cap.read()
 
-    ret, buff = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
-    string_data = buff.tobytes()
-    print(getsizeof(string_data))
-    frag.fragment(string_data)
+        ret2, buff = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
+        string_data = pickle.dumps(buff)
+        frag.fragment(string_data)
 
 
 if __name__ == "__main__":
--- reconstruct.py
+++ reconstruct.py
@@ -1,12 +1,11 @@
 import socket
 import threading
-import time
-import numpy as np
 import cv2
+import pickle
 
 class Reconstruct():
     """listen for messages over udp port and reconstruct them back into the original images"""
-    def __init__(self, ip="127.0.0.1", port=5005, packsize=6000):
+    def __init__(self, ip="127.0.0.1", port=5005, packsize=100000):
         self.msgList = []
         self.ip = ip
         self.port = port
@@ -14,32 +13,24 @@ class Reconstruct():
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.ip, self.port))
         self.listen = threading.Thread(target=self.listener)
+        self.data = None
 
     def listener(self):
-        msgList = []
-        start = time.time()
-        while (time.time() - start) < 12:
-            data, addr = self.sock.recvfrom(self.packsize)
-            # delimit between frames
-            """if data.decode() == "xxx":
-                #print("new frame")
-                pass"""
-            self.msgList.append(np.frombuffer(data, dtype=np.uint8))
+        while True:
+            self.data, addr = self.sock.recvfrom(self.packsize)
 
     def reconstruct(self):
-        if self.msgList:
-            print(len(self.msgList))
-            data = np.concatenate(self.msgList)
-            data = np.frombuffer(data, dtype=np.uint8)
-            image = cv2.imdecode(data, flags=cv2.IMREAD_COLOR)
-            cv2.imshow("image test", image)
-            cv2.waitKey(0)
+        while True:
+            if self.data is not None:
+                data = pickle.loads(self.data)
+                image = cv2.imdecode(data, flags=cv2.IMREAD_COLOR)
+                cv2.imshow("image test", image)
+                cv2.waitKey(30)
 
 
 def main():
     recon = Reconstruct()
     recon.listen.start()
-    time.sleep(12)
     recon.reconstruct()
 
 
